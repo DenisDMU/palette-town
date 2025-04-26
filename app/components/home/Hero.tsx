2346469c7ee348a79b9683188c32a54d9c2bab4b
Denis M <146975509+DenisDMU@users.noreply.github.com>
2025-04-26 12:24:05 +0200

    deploy: fixed by removing import useRef

--- app/components/home/Hero.tsx
+++ app/components/home/Hero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef} from "react";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { LucideSearch } from "lucide-react";
